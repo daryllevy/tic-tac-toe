c2a52680f01ca08cde027e43920360793f65ffea
Daryl Levy <222512535+daryllevy@users.noreply.github.com>
2026-06-15 13:02:09 +0100

    feat: ajout de la fonction qui place les marqueurs dans la grille

--- script.js
+++ script.js
@@ -1,140 +1,53 @@
 "use strict";
 
-// Le gameboard
+// <-- Etapes pour créer mon Tic Tac Toe : -->
+
+// 4. Je vérifie si le jeu est terminé
+// 5. Je vérifie si on a un gagnant ou un match nul
+
+// 1. Je crée la grille
+// Mon objet qui crée la grille
 const gameBoard = (() => {
   const row = 3;
   const column = 3;
-  //   Le tableau
   let board = [];
 
-  //   Pour créer la grille
   for (let i = 0; i < row; i++) {
     board[i] = [];
     for (let j = 0; j < column; j++) {
-      board[i].push(columnCell());
+      board[i].push(cell());
     }
   }
 
   const getBoard = () => board;
 
-  const placeMarker = (row, col, player) => {
-    if (board[row][col].getValue() !== 0) return false;
+  // 3. Je place les pions sur la grille
+  const placeMarker = (player, row, col) => {
+    if (board[row][col].getValue() != 0) {
+      console.log("Case déjà occupé");
+      return;
+    }
 
     board[row][col].setValue(player.marker);
-    return true;
   };
 
+  console.log(board);
   return { getBoard, placeMarker };
 })();
 
-// L'objet Player
+// 2. Je créer les joueurs
+// Mon objet qui crée les joueurs
 const player = (marker) => {
   let score = 0;
 
-  const addScore = () => {
-    score++;
-  };
-
   const getScore = () => score;
+  const addScore = () => score++;
 
-  return { marker, addScore, getScore };
+  return { marker, getScore, addScore };
 };
 
-// Le flux de jeu
-const gameController = (player1, player2, board) => {
-  let activePlayer = player1;
-  let boardFull = 0;
-
-  // Fonction pour déterminer le gagnant
-  function winner(board, player) {
-    // Combinaisons de chemins gagnant
-    const winningPaths = [
-      [
-        [0, 0],
-        [0, 1],
-        [0, 2],
-      ],
-      [
-        [1, 0],
-        [1, 1],
-        [1, 2],
-      ],
-      [
-        [2, 0],
-        [2, 1],
-        [2, 2],
-      ],
-      [
-        [0, 0],
-        [1, 0],
-        [2, 0],
-      ],
-      [
-        [0, 0],
-        [1, 1],
-        [2, 2],
-      ],
-      [
-        [0, 1],
-        [1, 1],
-        [2, 1],
-      ],
-      [
-        [0, 2],
-        [1, 2],
-        [2, 2],
-      ],
-      [
-        [0, 2],
-        [1, 1],
-        [2, 1],
-      ],
-    ];
-
-    // Si il ya une combinaison gagnante
-    // Si il y a une row dont tous les chemins sont identiques
-    return winningPaths.some((row) => {
-      return row.every((path) => {
-        let r = path[0];
-        let c = path[1];
-        let cell = board[r][c];
-
-        return cell.getValue() === player.marker;
-      });
-    });
-  }
-
-  // Fonction pour jouer un round
-  const playRound = (row, col) => {
-    // Je place le marker
-    const wasPlaced = board.placeMarker(row, col, activePlayer);
-
-    // Je vérifie qu'on a pas cliqué sur une case occupée
-    if (wasPlaced === false) return;
-
-    // J'incrémente le compteur de coup
-    boardFull++;
-
-    // Je verifie maintenant si il y a un gagnant
-    if (winner(board.getBoard(), activePlayer)) {
-      return `${activePlayer.marker}`;
-    }
-
-    // vérifier si le board est plein
-    if (boardFull === 9) {
-      return `It's a tie!`;
-    }
-
-    activePlayer === player1
-      ? (activePlayer = player2)
-      : (activePlayer = player1);
-  };
-
-  return { playRound };
-};
-
-// Fonction pour créer une case
-function columnCell() {
+// Fonction pour créer une cellule
+function cell() {
   let value = 0;
 
   const setValue = (val) => {
